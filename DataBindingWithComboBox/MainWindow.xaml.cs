97776677069bd37a0f2af6276851b0e37e1d11cf
devMFK <mfurkan.keskin2@gmail.com>
2022-08-19 14:50:11 +0300

    Add project files.

---
+++ DataBindingWithComboBox/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingWithComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() //constructor program açılırken
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+            MessageBox.Show("Program açıldı");
+            //string[] comboItems = new[] { "Kola", "Ayran", "Çay" };
+            ObservableCollection<Mesrubat> icecekler = new ObservableCollection<Mesrubat>();
+            icecekler.Add(new Mesrubat("Kola", 15));
+            icecekler.Add(new Mesrubat("Ayran", 11));
+            icecekler.Add(new Mesrubat("Soda", 7));
+            icecekler.Add(new Mesrubat("kefir", 20));
+            OrnekComboBox.ItemsSource = icecekler;
+            OrnekComboBox.DisplayMemberPath = "Ad"; //anahtar key ad
+            OrnekComboBox.SelectedValuePath = "Fiyat"; //key value ilişkisi valuemuz fiyat
+            MessageBox.Show("Program kapandı");
+        }
+
+        private void OrnekButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrnekLabel.Content = OrnekComboBox.SelectedValue;
+        }
+    }
+}
---
+++ DataBindingWithComboBox/Mesrubat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingWithComboBox
+{
+    public class Mesrubat
+    {
+        public string Ad { get; set; }
+        public int Fiyat { get; set; }
+
+        public Mesrubat(string _ad, int _fiyat)
+        {
+            Ad = _ad;
+            Fiyat = _fiyat;
+        }
+    }
+}
